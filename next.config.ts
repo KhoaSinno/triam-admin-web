e063f5ff89cc3f1c7410e27a9a63309e84453966
Sinoo <khoasinno@gmail.com>
2026-08-13 16:55:39 +0700

    update proxy

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/backend/:path*",
+        destination: "http://ec2-54-179-190-142.ap-southeast-1.compute.amazonaws.com:8000/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
